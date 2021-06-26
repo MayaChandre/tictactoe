6132598d61df02fd12b5348e71b290c161334bf6
mayavathi <mayachandre27@gmail.com>
2021-06-25 22:51:21 -0400

    creating a branch

--- src/com/bridgelabz/tictactoe.java
+++ src/com/bridgelabz/tictactoe.java
@@ -1,5 +1,16 @@
 package com.bridgelabz;
 
 public class tictactoe {
-
+private static char[] Board() {
+		char[] boardArray = new char[11];
+		for(char c : boardArray) {
+			c = ' ';
+		}
+		return boardArray;
+	}
+	
+	public static void main(String[] args) {
+		Board();
+	}
 }
+
